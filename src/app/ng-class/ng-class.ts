0c8ebc38df067a9c0bd6a7d260b19d5405e33bf9
joao_m_lessa <joao_m_lessa@estudante.sc.senai.br>
2026-04-23 20:07:56 -0300

    add initial components and templates for menu, ng-class, ng-for, ng-if, ng-model, ng-style, ng-switch, and pipes

---
+++ src/app/ng-class/ng-class.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-class',
+  imports: [],
+  templateUrl: './ng-class.html',
+  styleUrl: './ng-class.scss',
+})
+export class NgClass {
+
+}
