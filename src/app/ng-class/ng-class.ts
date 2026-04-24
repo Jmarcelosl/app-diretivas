4422a36e5d4749550792a56757f9a03bde550c07
joao_m_lessa <joao_m_lessa@estudante.sc.senai.br>
2026-04-23 21:53:27 -0300

    feat: add routing for directive components and implement menu navigation
    - Added routes for ng-class, ng-for, ng-if, ng-model, ng-style, ng-switch, and pipes components in app.routes.ts.
    - Removed app.spec.ts as it was no longer needed.
    - Updated app.ts to include the Menu component.
    - Created a navigation menu in menu.html with links to the new routes.
    - Styled the menu with menu.scss for better visual appearance.
    - Removed unused spec files for menu, ng-class, ng-for, ng-if, ng-model, ng-style, ng-switch, and pipes.
    - Updated ng-class component to demonstrate dynamic class binding with buttons.
    - Added styles for dynamic classes in ng-class.scss.
    - Included Bootstrap CSS and JS in index.html for styling and functionality.

--- src/app/ng-class/ng-class.ts
+++ src/app/ng-class/ng-class.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-class',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ng-class.html',
   styleUrl: './ng-class.scss',
 })
 export class NgClass {
-
+  public valor: boolean = true; 
 }
